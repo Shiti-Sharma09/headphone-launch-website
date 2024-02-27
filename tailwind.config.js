5f7991d9a77abcbf8bd7f5b18b211bd64a16c40f
Shiti Sharma <113706711+Shiti-Sharma09@users.noreply.github.com>
2024-02-27 18:53:32 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'wave-pattern': "url('/img/wave.jpg')", // Define the custom background image
+      }
+    },
   },
   plugins: [],
 }
